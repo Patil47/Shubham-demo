7b2041ce27dbbc28443c3f16a477e9adb459c502
Shubham Patil <128214072+Patil47@users.noreply.github.com>
2025-01-07 16:27:25 +0530

    Update Cricketer.java

--- Cricketer.java
+++ Cricketer.java
@@ -1,5 +1,7 @@
 public class Cricketer{
     public static void main(String[] args) {
         System.out.println("Hello Cricketer!");
+        
+        System.out.println("Hello Indian Cricket");
     }
 }
